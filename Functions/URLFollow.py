463fa2a169984879aa5904d00f154492cf1c240d
Matthew Cox <matthewcpcox@gmail.com>
2013-06-13 22:58:01 +0100

    move extension check after the imgur check

--- Functions/URLFollow.py
+++ Functions/URLFollow.py
@@ -19,16 +19,13 @@ class Instantiate(Function):
         if not match:
             return
         
-        if re.search('\.(jpe?g|gif|png|bmp)$', match.group('url')):
-            return
-        
         youtubeMatch = re.search('(www\.youtube\.com/watch.+v=|youtu\.be/)(?P<videoID>[^&#]+)', match.group('url'))
-        imgurMatch   = re.search('(i\.)?imgur.com/(?P<imgurID>[^\.]+)', match.group('url'))
+        imgurMatch   = re.search('(i\.)?imgur\.com/(?P<imgurID>[^\.]+)', match.group('url'))
         if youtubeMatch:
             return self.FollowYouTube(youtubeMatch.group('videoID'), message)
         elif imgurMatch:
             return self.FollowImgur(imgurMatch.group('imgurID'), message)
-        else:
+        elif not re.search('\.(jpe?g|gif|png|bmp)$', match.group('url')):
             return self.FollowStandard(match.group('url'), message)
         
     def FollowYouTube(self, videoID, message):
