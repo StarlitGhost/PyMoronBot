29133182f552038f417051decf376f50b790d535
Matthew Cox <matthewcpcox@gmail.com>
2013-06-16 18:12:35 +0100

    I'm beginning to wonder if this will ever 'just work'

--- IRCResponse.py
+++ IRCResponse.py
@@ -5,6 +5,6 @@ ResponseType = enum('Say','Do','Notice','Raw')
 class IRCResponse:
     def __init__(self, messageType, response, target):
         self.Type = messageType
-        self.Response = unicode(response)
+        self.Response = unicode(response, 'utf-8')
         self.Target = target
 
