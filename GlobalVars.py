2d3b34c3150a3d243319bb6a90ff802450dae260
Matthew Cox <matthewcpcox@gmail.com>
2013-12-09 09:24:37 +0000

    tidy up GlobalVars.py a little

--- GlobalVars.py
+++ GlobalVars.py
@@ -3,13 +3,14 @@ import os
 functions = {}
 
 CurrentNick = 'PyMoronBot'
-server = 'irc.desertbus.org'#'irc.applejack.me'#'irc.desertbus.org'
+
+server = 'irc.desertbus.org'
 port = 6667
-channels = ['#survivors']#, '#desertbus', '#unmoderated', '#help', '#games']
+channels = ['#desertbus', '#unmoderated', '#help', '#games']
 admins = ['Tyranic-Moron', 'T-M|Work', 'Tyranic_Moron', 'T-M|Asleep', 'LordCustardSmingleigh', 'HappyHAL9k', 'XelaReko', 'XelaReco', 'dave_random', 'ElementalAlchemist', 'Homoglyph']
 
 finger = 'GET YOUR FINGER OUT OF THERE'
-version = '0.0.1'
+version = '0.0.2'
 source = 'https://github.com/MatthewCox/PyMoronBot/'
 
 CommandChar = '\\'
