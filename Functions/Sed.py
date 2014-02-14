ba36d996dd2749064ade60036c2799b6a4393a76
Matthew Cox <matthewcpcox@gmail.com>
2014-02-14 13:20:54 +0000

    Add sed function

---
+++ Functions/Sed.py
@@ -0,0 +1,25 @@
+'''
+Created on Feb 14, 2014
+
+@author: Tyranic-Moron
+'''
+
+from IRCMessage import IRCMessage
+from IRCResponse import IRCResponse, ResponseType
+from Function import Function
+from GlobalVars import *
+
+import re
+
+# matches a sed-style regex pattern (taken from https://github.com/mossblaser/BeardBot/blob/master/modules/sed.py)
+# I stripped the unnecessary escapes by using a raw string instead
+sedRegex = r"s/(?P<search>(\\\\|(\\[^\\])|[^\\/])+)/(?P<replace>(\\\\|(\\[^\\])|[^\\/])*)((/(?P<flags>.*))?)"
+
+class Instantiate(Function):
+    Help = 'matches sed-like regex patterns and attempts to execute them on the latest matching line from the last 10'
+
+    def GetResponse(self, message):
+        if message.Type != 'PRIVMSG':
+            return
+        
+        pass #TODO: everything!
