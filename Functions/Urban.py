36f582a53a3b3bca9aa97ce22a1ebf13162eb7f8
Matthew Cox <matthewcpcox@gmail.com>
2014-02-15 17:27:01 +0000

    [Urban] Replace links with text in def and example

--- Functions/Urban.py
+++ Functions/Urban.py
@@ -35,31 +35,45 @@ class Instantiate(Function):
         webPage = WebUtils.FetchURL(url)
 
         soup = BeautifulSoup(webPage.Page)
+        # replace link tags with their contents
+        [a.unwrap() for a in soup.find_all('a')]
 
         box = soup.find('div', {'class' : 'box'})
 
         if not box:
             return IRCResponse(ResponseType.Say, "No entry found for '{0}'".format(search), message.ReplyTo)
+        
+        print box.find('div', {'class' : 'meaning'}).text
 
         graySplitter = assembleFormattedText(A.normal[' ', A.fg.gray['|'], ' '])
 
         word = box.find('div', {'class' : 'word'}).text.strip()
+
         # 2014-01-28 really, urban dictionary? 'definition' to 'meaning'? what an important change!
-        definition = graySplitter.join(box.find('div', {'class' : 'meaning'}).stripped_strings)
-        example = graySplitter.join(box.find('div', {'class' : 'example'}).stripped_strings)
+        definition = box.find('div', {'class' : 'meaning'})
+        if definition.br is not None:
+            definition.br.replace_with('\n')
+        definition = graySplitter.join([s.strip() for s in definition.text.strip().split('\n')])
+
+        example = box.find('div', {'class' : 'example'})
+        if example.br is not None:
+            example.br.replace_with('\n')
+        example = graySplitter.join([s.strip() for s in example.text.strip().split('\n')])
+
         author = box.find('div', {'class' : 'contributor'}).text.strip().replace('\n', ' ')
+
         counts = box.find('div', {'class' : 'thumbs-counts'}).find_all('span', {'class' : 'count'})
         up = counts[0].text
         down = counts[1].text
 
         if word.lower() != message.Parameters.lower():
-            word = "Contains '{0}'".format(word)
+            word = "{0} (Contains '{0}')".format(word, message.Parameters)
 
         formatString = assembleFormattedText(
                            A.normal[A.bold["{0}:"], " {1}\n",
                                     A.bold["Example(s):"], " {2}\n",
                                     "{3}", graySplitter,
-                                    A.fg.lightGreen["+{4}"], "/", A.fg.lightRed["-{5}"], graySplitter, "More defs: {6}"])
+                                    A.fg.lightGreen["+{4}"], A.fg.gray["/"], A.fg.lightRed["-{5}"], graySplitter, "More defs: {6}"])
 
         response = formatString.format(word, definition, example, author, up, down, url)
         
