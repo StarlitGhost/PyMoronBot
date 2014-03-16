e6739050cd9818cb0753f3c60e7ed0ea633dff99
Matthew Cox <matthewcpcox@gmail.com>
2014-03-16 21:55:23 +0000

    [URLFollow] fix KS follow for urls with ref

--- Functions/URLFollow.py
+++ Functions/URLFollow.py
@@ -31,15 +31,15 @@ class Instantiate(Function):
             if message.User.Name in ignores.ignoreList:
                 return
 
-        match = re.search('(?P<url>https?://[^\s]+)', message.MessageString, re.IGNORECASE)
+        match = re.search(r'(?P<url>https?://[^\s]+)', message.MessageString, re.IGNORECASE)
         if not match:
             return
         
-        youtubeMatch = re.search('(www\.youtube\.com/watch.+v=|youtu\.be/)(?P<videoID>[^&#]+)', match.group('url'))
-        imgurMatch   = re.search('(i\.)?imgur\.com/(?P<imgurID>[^\.]+)', match.group('url'))
-        twitterMatch = re.search('twitter.com/(?P<tweeter>[^/]+)/status/(?P<tweetID>[0-9]+)', match.group('url'))
-        steamMatch   = re.search('store.steampowered.com/app/(?P<steamAppID>[0-9]+)', match.group('url'))
-        ksMatch      = re.search('kickstarter.com/projects/(?P<ksID>[^/]+/[^/]+)', match.group('url'))
+        youtubeMatch = re.search(r'(www\.youtube\.com/watch.+v=|youtu\.be/)(?P<videoID>[^&#]+)', match.group('url'))
+        imgurMatch   = re.search(r'(i\.)?imgur\.com/(?P<imgurID>[^\.]+)', match.group('url'))
+        twitterMatch = re.search(r'twitter.com/(?P<tweeter>[^/]+)/status/(?P<tweetID>[0-9]+)', match.group('url'))
+        steamMatch   = re.search(r'store.steampowered.com/app/(?P<steamAppID>[0-9]+)', match.group('url'))
+        ksMatch      = re.search(r'kickstarter.com/projects/(?P<ksID>[^/]+/[^/\?\#\&]+)', match.group('url'))
         
         if youtubeMatch:
             return self.FollowYouTube(youtubeMatch.group('videoID'), message)
