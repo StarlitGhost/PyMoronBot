e0751998975001add66abdee9bb654863907f905
Heufneutje <stefan.frijters@hotmail.com>
2014-04-16 09:32:28 +0000

    I forgot Python can iterate strings

--- ServerInfo.py
+++ ServerInfo.py
@@ -1,11 +1,11 @@
 class ServerInfo:
 
     # I have used the defaults specified by RFC1459 here
-    SupportedUserModes = ['i', 'o', 's', 'w']
-    SupportedChannelListModes = ['b']
-    SupportedChannelSetArgsModes = ['l']
-    SupportedChannelSetUnsetArgsModes = ['k']
-    SupportedChannelNormalModes = ['i','m','n','p','s','t']
+    SupportedUserModes = 'iosw'
+    SupportedChannelListModes = 'b'
+    SupportedChannelSetArgsModes = 'l'
+    SupportedChannelSetUnsetArgsModes = 'k'
+    SupportedChannelNormalModes = 'imnpst'
 
     SupportedStatuses = {'o': '@', 'v': '+'}
     SupportedStatusesReverse = {'@': 'o', '+': 'v'}
