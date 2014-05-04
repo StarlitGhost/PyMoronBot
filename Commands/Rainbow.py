0e471c1df729c4eaa1fb5599422652d1ef08db89
Matthew Cox <matthewcpcox@gmail.com>
2014-05-04 18:42:33 +0100

    Create the Rainbow command!

---
+++ Commands/Rainbow.py
@@ -0,0 +1,37 @@
+'''
+Created on May 04, 2014
+
+@author: Tyranic-Moron
+'''
+
+from IRCMessage import IRCMessage, IRCChannel
+from IRCResponse import IRCResponse, ResponseType
+from CommandInterface import CommandInterface
+
+from twisted.words.protocols.irc import assembleFormattedText, attributes as A
+
+class Command(CommandInterface):
+    triggers = ['rainbow']
+    help = 'rainbow <text> - outputs the specified text with rainbow colours'
+
+    colours = [assembleFormattedText(A.fg.lightRed['']),
+               #assembleFormattedText(A.fg.orange['']),
+               assembleFormattedText(A.fg.yellow['']),
+               assembleFormattedText(A.fg.lightGreen['']),
+               assembleFormattedText(A.fg.lightCyan['']),
+               assembleFormattedText(A.fg.lightBlue['']),
+               assembleFormattedText(A.fg.lightMagenta['']),
+               ]
+
+    def execute(self, message=IRCMessage):
+        if len(message.ParameterList) == 0:
+            return IRCResponse(ResponseType.Say, "You didn't give me any text to rainbow!", message.ReplyTo)
+
+        outputMessage = ''
+
+        for i, c in enumerate(message.Parameters):
+            outputMessage += self.colours[i % len(self.colours)] + c
+
+        outputMessage += assembleFormattedText(A.normal[''])
+
+        return IRCResponse(ResponseType.Say, outputMessage, message.ReplyTo)
