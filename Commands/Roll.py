890b29ab2c91335d5651abbe8519a5853d7ddd64
Matthew Cox <matthewcpcox@gmail.com>
2014-05-10 07:54:02 +0100

    Add a Roll command using the PLY python library. Very much like old MoronBot's. Closes #55

---
+++ Commands/Roll.py
@@ -0,0 +1,211 @@
+"""
+Created on Dec 05, 2013
+
+@author: Tyranic-Moron
+"""
+import random
+
+from IRCMessage import IRCMessage
+from IRCResponse import IRCResponse, ResponseType
+from CommandInterface import CommandInterface
+
+import operator
+import ply.lex as lex
+import ply.yacc as yacc
+
+
+class UnknownCharacterException(Exception):
+    pass
+
+class SyntaxErrorException(Exception):
+    pass
+
+class TooManyDiceException(Exception):
+    pass
+
+class TooManySidesException(Exception):
+    pass
+
+class NegativeDiceException(Exception):
+    pass
+
+class NegativeSidesException(Exception):
+    pass
+
+
+class Command(CommandInterface):
+    triggers = ['roll', 'rollv']
+    help = 'roll(v) - dice roller, remind me to write more here later!'
+
+    def onStart(self):
+
+        tokens = ('NUMBER',
+                  'PLUS', 'MINUS', 'TIMES', 'DIVIDE', 'DICE',
+                  'LPAREN', 'RPAREN')
+
+        # Tokens
+
+        t_PLUS = r'\+'
+        t_MINUS = r'-'
+        t_TIMES = r'\*'
+        t_DIVIDE = r'/'
+        t_DICE = r'd'
+        t_LPAREN = r'\('
+        t_RPAREN = r'\)'
+
+        def t_NUMBER(t):
+            r"""\d+"""
+            try:
+                t.value = int(t.value)
+            except ValueError:
+                t.value = 0
+            return t
+
+        # Ignored characters
+        t_ignore = ' \t'
+
+        def t_COMMENT(t):
+            r"""\#.*"""
+            pass
+            # No return value. Token discarded
+
+        # Calculate the column position of the given token.
+        #     input is the input text string
+        #     token is a token instance
+        def findColumn(token):
+            if token is not None:
+                last_cr = self.lexer.lexdata.rfind('\n', 0, token.lexpos)
+                if last_cr < 0:
+                    last_cr = 0
+                column = (token.lexpos - last_cr) + 1
+                return column
+            else:
+                return 'unknown'
+
+        def t_error(t):
+            col = findColumn(t)
+            raise UnknownCharacterException(u"unknown character '{0}' (col {1})".format(t.value[0], col))
+
+        self.lexer = lex.lex()
+
+
+        # Parsing rules
+        precedence = (('left', 'PLUS', 'MINUS'),
+                      ('left', 'TIMES', 'DIVIDE'),
+                      ('left', 'DICE'),
+                      ('right', 'UMINUS'),
+                      ('right', 'UDICE'))
+
+        def p_statement_expr(p):
+            """statement : expression"""
+            p[0] = p[1]
+
+        def p_expression_binop(p):
+            """expression : expression PLUS expression
+                          | expression MINUS expression
+                          | expression TIMES expression
+                          | expression DIVIDE expression"""
+            if p[2] == '+':
+                p[0] = operator.add(p[1], p[3])
+            elif p[2] == '-':
+                p[0] = operator.sub(p[1], p[3])
+            elif p[2] == '*':
+                p[0] = operator.mul(p[1], p[3])
+            elif p[2] == '/':
+                p[0] = operator.div(p[1], p[3])
+
+        def p_expression_dice(p):
+            """expression : expression DICE expression"""
+            p[0] = rollDice(p[1], p[3])
+
+        def p_expression_uminus(p):
+            """expression : MINUS expression %prec UMINUS"""
+            p[0] = operator.neg(p[2])
+
+        def p_expression_udice(p):
+            """expression : DICE expression %prec UDICE"""
+            p[0] = rollDice(1, p[2])
+
+        def p_expression_group(p):
+            """expression : LPAREN expression RPAREN"""
+            p[0] = p[2]
+
+        def p_expression_number(p):
+            """expression : NUMBER"""
+            p[0] = p[1]
+
+        def p_error(p):
+            if p is None:
+                raise SyntaxErrorException(u"syntax error at the end of the given expression")
+
+            col = findColumn(p)
+            raise SyntaxErrorException(u"syntax error at '{0}' (col {1})".format(p.value, col))
+
+        def rollDice(numDice, numSides):
+            if numDice > 1000:
+                raise TooManyDiceException(u'attempted to roll more than 1000 dice in a single d expression')
+            if numSides > 1000:
+                raise TooManySidesException(u'attempted to roll a dice with more than 1000 sides')
+            if numDice < 0:
+                raise NegativeDiceException(u'attempted to roll a negative number of dice')
+            if numSides < 0:
+                raise NegativeSidesException(u'attempted to roll a dice with a negative number of sides')
+
+            rolls = []
+            for dice in xrange(0, numDice):
+                rolls.append(random.randint(1, numSides))
+
+            self.yaccer.rolls.append((u'{0}d{1}'.format(numDice, numSides), rolls))
+
+            return sum(rolls)
+
+        self.yaccer = yacc.yacc()
+        self.yaccer.rolls = []
+
+    def execute(self, message=IRCMessage):
+
+        verbose = False
+        if message.Command.lower().endswith('v'):
+            verbose = True
+
+        try:
+            result = self.yaccer.parse(message.Parameters)
+        except OverflowError:
+            self.yaccer.rolls = []
+
+            return IRCResponse(ResponseType.Say,
+                               u'Error: result too large to calculate',
+                               message.ReplyTo)
+        except (ZeroDivisionError,
+                UnknownCharacterException,
+                SyntaxErrorException,
+                TooManyDiceException,
+                TooManySidesException,
+                NegativeDiceException,
+                NegativeSidesException) as e:
+            self.yaccer.rolls = []
+
+            return IRCResponse(ResponseType.Say,
+                               u'Error: {0}'.format(e),
+                               message.ReplyTo)
+
+        rolls = self.yaccer.rolls
+        self.yaccer.rolls = []
+
+        if verbose:
+            rollStrings = []
+            for dice, rollList in rolls:
+                rollStrings.append(u'{0}: {1} ({2})'.format(dice,
+                                                            u','.join(u'{0}'.format(roll) for roll in rollList),
+                                                            sum(rollList)))
+            rollString = u' | '.join(rollStrings)
+
+            if len(rollString) > 200:
+                rollString = u"LOTS O' DICE"
+
+            response = u'{0} rolled: [{1}] {2}'.format(message.User.Name, rollString, result)
+
+        else:
+            response = u'{0} rolled: {1}'.format(message.User.Name, result)
+
+        return IRCResponse(ResponseType.Say, response, message.ReplyTo)
