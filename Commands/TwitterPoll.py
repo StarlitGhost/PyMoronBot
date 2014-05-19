a4c20eb0072e7ddddb8ae6977973f84a1e3c701b
Matthew Cox <matthewcpcox@gmail.com>
2014-05-19 21:16:35 +0100

    [TwitterPoll] skip replies, add the subcommands. user query not done yet.

--- Commands/TwitterPoll.py
+++ Commands/TwitterPoll.py
@@ -11,7 +11,7 @@ from urllib2 import Request, urlopen
 import datetime
 from twisted.internet import task, threads
 from twisted.words.protocols.irc import assembleFormattedText, attributes as A
-from twitter import Twitter, OAuth2
+from twitter import Twitter, OAuth2, TwitterHTTPError
 
 from CommandInterface import CommandInterface
 from Data import api_keys
@@ -29,18 +29,19 @@ class TwitterPoll(CommandInterface):
     runInThread = True
 
     def onLoad(self):
-        self.follows = {'desertbus': datetime.datetime.utcnow(),
-                        'official_pax': datetime.datetime.utcnow(),
-                        'loadingreadyrun': datetime.datetime.utcnow(),
-                        'lrrmtg': datetime.datetime.utcnow(),
-                        'tyranicmoron': datetime.datetime.utcnow()}
+        self.follows = {u'desertbus': datetime.datetime.utcnow(),
+                        u'official_pax': datetime.datetime.utcnow(),
+                        u'loadingreadyrun': datetime.datetime.utcnow(),
+                        u'lrrmtg': datetime.datetime.utcnow(),
+                        u'tyranicmoron': datetime.datetime.utcnow()}
 
         bearer_token = self._get_access_token()
         self.twitter = Twitter(auth=OAuth2(bearer_token=bearer_token))
 
         # start the thread that checks for new tweets
         self.scanner = task.LoopingCall(self._scanLoop)
-        self.scanner.start(120, now=False)
+
+        self._restartScanner()
 
     def onUnload(self):
         if self.scanner.running:
@@ -51,7 +52,92 @@ class TwitterPoll(CommandInterface):
         @type message: IRCMessage
         """
         # do the command stuff described in the help
-        pass
+        if len(message.ParameterList) == 0:
+            return IRCResponse(ResponseType.Say, self.help, message.ReplyTo)
+
+        subCommand = message.ParameterList[0].lower()
+
+        if subCommand == 'follow':
+            new, existing, invalid = self._follow(message.ParameterList[1:])
+            responses = []
+            if len(new) > 0:
+                responses.append(IRCResponse(ResponseType.Say,
+                                             'Now following: {0}'.format(', '.join(new)),
+                                             message.ReplyTo))
+            if len(existing) > 0:
+                responses.append(IRCResponse(ResponseType.Say,
+                                             'I am already following: {0}'.format(', '.join(existing)),
+                                             message.ReplyTo))
+            if len(invalid) > 0:
+                responses.append(IRCResponse(ResponseType.Say,
+                                             "These aren't valid twitter accounts: {0}".format(', '.join(invalid)),
+                                             message.ReplyTo))
+            return responses
+        elif subCommand == 'unfollow':
+            removed, nonexistent = self._unfollow(message.ParameterList[1:])
+            responses = []
+            if len(removed) > 0:
+                responses.append(IRCResponse(ResponseType.Say,
+                                             'Unfollowed: {0}'.format(', '.join(removed)),
+                                             message.ReplyTo))
+            if len(nonexistent) > 0:
+                responses.append(IRCResponse(ResponseType.Say,
+                                             "I'm already not following: {0}".format(', '.join(nonexistent)),
+                                             message.ReplyTo))
+        else:
+            # TODO fetch latest tweet from specified user
+            pass
+
+    def _follow(self, users):
+        """
+        @type users: list[unicode]
+        """
+        new = []
+        existing = []
+        invalid = []
+        for user in users:
+            if user.lower() in self.follows:
+                existing.append(user.lower())
+            else:
+                # TODO check user is valid somehow
+                if self._checkUserExists(user):
+                    new.append(user)
+                    self.follows[user] = datetime.datetime.utcnow()
+                else:
+                    invalid.append(user)
+
+        self._restartScanner()
+
+        return new, existing, invalid
+
+    def _unfollow(self, users):
+        """
+        @type users: list[unicode]
+        """
+        removed = []
+        nonexistent = []
+        for user in users:
+            if user.lower() in self.follows:
+                removed.append(user.lower())
+                del self.follows[user.lower()]
+            else:
+                nonexistent.append(user)
+
+        self._restartScanner()
+
+        return removed, nonexistent
+
+    def _checkUserExists(self, user):
+        try:
+            self.twitter.users.show(screen_name=user)
+            return True
+        except TwitterHTTPError:
+            return False
+
+    def _restartScanner(self):
+        if self.scanner.running:
+            self.scanner.stop()
+        self.scanner.start(((60 * 15) / 300) * len(self.follows) + 5, now=False)
 
     def _scanLoop(self):
         return threads.deferToThread(self._scanTwitter)
@@ -77,6 +163,10 @@ class TwitterPoll(CommandInterface):
                 newTweets = newTweets[::-1]  # reverse the list so oldest tweets are first
 
                 for tweet in newTweets:
+                    # skip replies
+                    if tweet['in_reply_to_screen_name'] is not None:
+                        continue
+
                     tweetText = re.sub('[\r\n]+', StringUtils.graySplitter, tweet['text'])
                     for url in tweet['entities']['urls']:
                         tweetText = tweetText.replace(url['url'], url['expanded_url'])
