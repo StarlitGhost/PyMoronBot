7d223717612e8b42cbc185c664e598aed2ebdd98
Matthew Cox <matthewcpcox@gmail.com>
2014-12-07 02:07:14 +0000

    Add a Manhole module using Twisted's Manhole

---
+++ Commands/Manhole.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Dec 07, 2014
+
+@author: Tyranic-Moron
+"""
+
+from CommandInterface import CommandInterface
+
+from twisted.conch.manhole_tap import makeService
+import os
+
+
+class Manhole(CommandInterface):
+    help = "A debug module that uses Twisted's Manhole to poke at the bot's innards"
+    runInThread = True
+
+    manhole = None
+
+    def onLoad(self):
+        self.manhole = makeService({
+            "namespace": {"bot": self.bot},
+            "passwd": os.path.join("Data", "manhole.passwd"),
+            "telnetPort": "4040",
+            "sshPort": None
+        })
+        self.manhole.startService()
+
+    def onUnload(self):
+        self.manhole.stopService()
