662f4ba651df3d764656d70eabce9da8b1913a19
Matthew Cox <matthewcpcox@gmail.com>
2014-12-07 00:32:16 +0000

    [Gif] add an optional year argument

--- Commands/Gif.py
+++ Commands/Gif.py
@@ -4,6 +4,7 @@ Created on Dec 05, 2013
 
 @author: Tyranic-Moron
 """
+import random
 
 from CommandInterface import CommandInterface
 from IRCMessage import IRCMessage
@@ -14,16 +15,35 @@ from Utils import WebUtils
 
 class Gif(CommandInterface):
     triggers = ['gif']
-    help = 'gif - fetches a random gif posted during Desert Bus'
+    help = 'gif [<year>] - fetches a random gif posted during Desert Bus'
     
     def execute(self, message):
         """
         @type message: IRCMessage
         """
-        url = "http://greywool.com/desertbus/7/gifs/random.php"
+
+        baseURL = "http://greywool.com/desertbus/{}/gifs/random.php"
+        years = range(7, 9)
+
+        if len(message.ParameterList) > 0:
+            invalid = u"'{}' is not a valid year, valid years are {} to {}"\
+                .format(message.ParameterList[0], years[0], years[-1])
+            try:
+                year = int(message.ParameterList[0])
+            except ValueError:
+                return IRCResponse(ResponseType.Say, invalid, message.ReplyTo)
+
+            if year not in years:
+                return IRCResponse(ResponseType.Say, invalid, message.ReplyTo)
+        else:
+            year = random.choice(years)
+
+        url = baseURL.format(year)
 
         webPage = WebUtils.fetchURL(url)
 
         link = webPage.body
 
-        return IRCResponse(ResponseType.Say, u"Random DB gif: {0}".format(link), message.ReplyTo)
+        return IRCResponse(ResponseType.Say,
+                           u"Random DB{} gif: {}".format(year, link),
+                           message.ReplyTo)
