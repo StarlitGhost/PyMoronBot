9ad4fd375703afe3454d5cb168533515e552385f
Matthew Cox <matthewcpcox@gmail.com>
2015-02-17 11:47:22 +0000

    Add AsterFix automatic module
    Attempts to use "*fix" patterns to fix the most likely word in your previous message

---
+++ Commands/AsterFix.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Feb 17, 2015
+
+@author: Tyranic-Moron
+"""
+
+import difflib
+import re
+
+from IRCMessage import IRCMessage
+from IRCResponse import IRCResponse
+from CommandInterface import CommandInterface
+
+
+class AsterFix(CommandInterface):
+    triggers = ['asterfix']
+    acceptedTypes = ['PRIVMSG', 'ACTION']
+
+    help = '*<fix> - looks for similar text in your last message and attempts to replace the most likely candidate'
+
+    def onLoad(self):
+        self.messages = {}
+
+    def shouldExecute(self, message):
+        """
+        @type message: IRCMessage
+        """
+        if message.Type in self.acceptedTypes:
+            return True
+
+    def execute(self, message):
+        """
+        @type message: IRCMessage
+        """
+        if message.Command.lower() in self.triggers:
+            messageString = message.Parameters
+        else:
+            messageString = message.MessageString
+
+        changeMatch = re.match(r"^\*(?P<change>[^\s*]+)$", messageString)
+        if changeMatch:
+            change = changeMatch.group('change')
+        else:
+            self.storeMessage(message)
+
+        lastMessage = self.messages[message.User.Name].MessageString.split()
+        likelyChanges = difflib.get_close_matches(change, lastMessage, 5, 0.5)
+        likelyChanges = filter((lambda word: word != change), likelyChanges)
+
+        if likelyChanges:
+            target = likelyChanges[0]
+
+            response = " ".join([change if word == target else word for word in lastMessage])
+
+            return IRCResponse(lastMessage.ResponseType, response, message.ReplyTo)
+
+    def storeMessage(self, message):
+        self.messages[message.User.Name] = message.MessageString
