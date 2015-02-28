807d01bdb9c9160bbf932ab4711a8a46dd3e5143
Matthew Cox <matthewcpcox@gmail.com>
2015-02-28 19:18:59 +0000

    replace mysql-python with pymysql, bump version number to 1.0.0

--- GlobalVars.py
+++ GlobalVars.py
@@ -4,10 +4,7 @@ import os
 admins = ['Tyranic-Moron', 'T-M|Work', 'Tyranic_Moron', 'T-M|Asleep', 'GarrusVakarian', 'LordCustardSmingleigh', 'XelaReko', 'XelaReco', 'Xel|Work', 'dave_random', 'ElementalAlchemist', 'Homoglyph', 'Heufy|Work', 'Heufneutje', 'HeufyDroid', 'HeufyCloud', 'HeufyTrain', 'HeufyBus', 'HubbeKing', 'HubbeWork', 'HubbeTrain', 'Mara', 'Didero', 'ekimekim']
 
 finger = 'GET YOUR FINGER OUT OF THERE'
-version = '0.7.0'  # 3 major features left to implement before I'll consider it 1.0.0 (listed below)
-# Unified command data storage (may decide not to bother, independent also works and has its own benefits)
-# Command Aliasing
-# Command Chaining
+version = '1.0.0'
 source = 'https://github.com/MatthewCox/PyMoronBot/'
 
 abspath = os.path.abspath(__file__)
