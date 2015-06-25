8b4b0fb150b1a9a6e8bb19db229093c3b739e7f8
MatthewCox <matthewcpcox@gmail.com>
2015-06-25 10:00:16 +0100

    [LRRChecker] remove old LRR urls

--- Data/LRRChecker.py
+++ Data/LRRChecker.py
@@ -5,30 +5,6 @@ aYearAgo = datetime.datetime.utcnow() - datetime.timedelta(days=365)
 tenMinsAgo = datetime.datetime.utcnow() - datetime.timedelta(minutes=10)
 
 LRRChecker = {
-    'Unskippable': {
-        'url': 'http://www.escapistmagazine.com/rss/videos/list/82.xml',
-        'lastUpdate': aYearAgo,
-        'lastTitle': '',
-        'lastLink': '',
-        'lastCheck': tenMinsAgo,
-        'aliases': ['unskipable', 'unskip', 'us', 'u', 'skip'],
-        'suppress': True},
-    'LRR': {
-        'url': 'http://www.escapistmagazine.com/rss/videos/list/123.xml',
-        'lastUpdate': aYearAgo,
-        'lastTitle': '',
-        'lastLink': '',
-        'lastCheck': tenMinsAgo,
-        'aliases': ["loadingreadyrun", "l", "llr"],
-        'suppress': True},
-    'Feed Dump': {
-        'url': 'http://www.escapistmagazine.com/rss/videos/list/171.xml',
-        'lastUpdate': aYearAgo,
-        'lastTitle': '',
-        'lastLink': '',
-        'lastCheck': tenMinsAgo,
-        'aliases': ["feed", "dump", "fdump", "fd", "f", 'fump', 'frump', 'feeddump'],
-        'suppress': True},
     'LRRCast': {
         'url': 'http://feeds.feedburner.com/lrrcast',
         'lastUpdate': aYearAgo,
