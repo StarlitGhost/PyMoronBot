a60aee88ec8d50d76788e79de8af2279a8e52386
Matthew Cox <matthewcpcox@gmail.com>
2015-08-31 03:48:36 +0100

    [Manhole] increment port if current one is in use, and return port when called by admin

--- Commands/Manhole.py
+++ Commands/Manhole.py
@@ -6,25 +6,42 @@ Created on Dec 07, 2014
 """
 
 from CommandInterface import CommandInterface
+from IRCMessage import IRCMessage
+from IRCResponse import IRCResponse, ResponseType
+import GlobalVars
 
 from twisted.conch.manhole_tap import makeService
+from twisted.internet.error import CannotListenError
 import os
 
 
 class Manhole(CommandInterface):
+    triggers = ['manhole']
     help = "A debug module that uses Twisted's Manhole to poke at the bot's innards"
     runInThread = True
 
     manhole = None
+    port = 4040
 
     def onLoad(self):
-        self.manhole = makeService({
-            "namespace": {"bot": self.bot},
-            "passwd": os.path.join("Data", "manhole.passwd"),
-            "telnetPort": None,
-            "sshPort": "4040"
-        })
-        self.manhole.startService()
+        while self.manhole is None or not self.manhole.running:
+            try:
+                self.manhole = makeService({
+                    "namespace": {"bot": self.bot},
+                    "passwd": os.path.join("Data", "manhole.passwd"),
+                    "telnetPort": None,
+                    "sshPort": str(self.port)
+                })
+                self.manhole.startService()
+            except CannotListenError:
+                self.port += 1
 
     def onUnload(self):
         self.manhole.stopService()
+
+    def execute(self, message):
+        """
+        @type message: IRCMessage
+        """
+        if message.User.Name in GlobalVars.admins:
+            return IRCResponse(ResponseType.Notice, "Manhole port: {}".format(self.port), message.User.Name)
