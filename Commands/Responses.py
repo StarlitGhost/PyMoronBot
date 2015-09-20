917aedf980fb76f5dcbfeb9b47958a00c71381ca
Matthew Cox <matthewcpcox@gmail.com>
2015-09-20 16:31:38 +0100

    [Responses] add a boop

--- Commands/Responses.py
+++ Commands/Responses.py
@@ -352,6 +352,7 @@ class Responses(CommandInterface):
                     "http://i.imgur.com/lVxFUGF.gifv",  # secret bag cat boops other cat on bed
                     "http://i.imgur.com/bLIBH6J.gifv",  # orange kitten eyes up another kitten, then pounces. En Garde!
                     "http://i.imgur.com/V35yPa0.gifv",  # 2 dogs boop their snoots in an expert manner
+                    "http://i.imgur.com/ihS28BF.gifv",  # dog boops a cat standing on the edge of a bath into it
                     ]
                 return [IRCResponse(ResponseType.Say,
                                     'Boop! {}'.format(random.choice(boops)),
