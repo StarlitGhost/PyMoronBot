52a107a76e921d91f3365cbd66e02beb2c5f389a
Matthew Cox <matthewcpcox@gmail.com>
2015-11-09 10:58:35 +0000

    [DominosPizza] fix the url it checks for

--- Commands/DominosPizza.py
+++ Commands/DominosPizza.py
@@ -19,7 +19,7 @@ class DominosPizza(CommandInterface):
         self.trackers = {}
         """@type : dict[str, TrackingDetails]"""
 
-        self.regex = r'www\.dominos\.(co\.uk|\.ie)/checkout/pizzatracker\.aspx\?id=(?P<orderID>[a-zA-Z0-9]+)'
+        self.regex = r'www\.dominos\.(co\.uk|\.ie)/PizzaTracker\?id=(?P<orderID>[a-zA-Z0-9]+)'
 
         commands = self.bot.moduleHandler.commands
         if 'URLFollow' in commands:
