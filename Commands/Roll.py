308367c378a7cdcc00f1fe75da07e38f8aad88d3
Matthew Cox <matthewcpcox@gmail.com>
2018-02-01 15:29:07 +0000

    [Roll] move dice rolling to DiceUtils

--- Commands/Roll.py
+++ Commands/Roll.py
@@ -5,43 +5,10 @@ Created on May 10, 2014
 @author: Tyranic-Moron
 """
 
-import random
-import operator
-import collections
-from builtins import range
-
 from IRCMessage import IRCMessage
 from IRCResponse import IRCResponse, ResponseType
 from CommandInterface import CommandInterface
-
-import ply.lex as lex
-import ply.yacc as yacc
-
-
-MAX_DICE = 10000
-MAX_SIDES = 10000
-
-
-class UnknownCharacterException(Exception):
-    pass
-
-class SyntaxErrorException(Exception):
-    pass
-
-class TooManyDiceException(Exception):
-    pass
-
-class TooManySidesException(Exception):
-    pass
-
-class NegativeDiceException(Exception):
-    pass
-
-class NegativeSidesException(Exception):
-    pass
-
-class ZeroSidesException(Exception):
-    pass
+from Utils import DiceUtils
 
 
 class Roll(CommandInterface):
@@ -52,152 +19,7 @@ class Roll(CommandInterface):
 
     # noinspection PyUnusedLocal
     def onLoad(self):
-
-        tokens = ('NUMBER',
-                  'PLUS', 'MINUS', 'TIMES', 'DIVIDE', 'EXPONENT', 'DICE',
-                  'LPAREN', 'RPAREN', 'POINT')
-
-        # Tokens
-
-        t_PLUS = r'\+'
-        t_MINUS = r'-'
-        t_TIMES = r'\*'
-        t_DIVIDE = r'/'
-        t_EXPONENT = r'\^'
-        t_DICE = r'd'
-        t_LPAREN = r'\('
-        t_RPAREN = r'\)'
-        t_POINT = r'\.'
-
-        def t_NUMBER(t):
-            r"""\d+"""
-            try:
-                if len(t.value) < 100:
-                    t.value = int(t.value)
-                else:
-                    raise ValueError
-            except ValueError:
-                t.value = 0
-            return t
-
-        # Ignored characters
-        t_ignore = ' \t'
-
-        def t_COMMENT(_):
-            r"""\#.*"""
-            pass
-            # No return value. Token discarded
-
-        # Calculate the column position of the given token.
-        #     input is the input text string
-        #     token is a token instance
-        def findColumn(token):
-            if token is not None:
-                last_cr = self.lexer.lexdata.rfind('\n', 0, token.lexpos)
-                if last_cr < 0:
-                    last_cr = 0
-                column = (token.lexpos - last_cr) + 1
-                return column
-            else:
-                return 'unknown'
-
-        def t_error(t):
-            col = findColumn(t)
-            raise UnknownCharacterException(u"unknown character '{0}' (col {1})".format(t.value[0], col))
-
-        self.lexer = lex.lex()
-
-        # Parsing rules
-        precedence = (('left', 'PLUS', 'MINUS'),
-                      ('left', 'TIMES', 'DIVIDE'),
-                      ('left', 'EXPONENT'),
-                      ('left', 'DICE'),
-                      ('right', 'UMINUS'),
-                      ('right', 'UDICE'))
-
-        def p_statement_expr(p):
-            """statement : expression"""
-            p[0] = p[1]
-
-        def p_expression_binop(p):
-            """expression : expression PLUS expression
-                          | expression MINUS expression
-                          | expression TIMES expression
-                          | expression DIVIDE expression
-                          | expression EXPONENT expression"""
-            op = p[2]
-            left = sumList(p[1])
-            right = sumList(p[3])
-
-            if op == '+':
-                p[0] = operator.add(left, right)
-            elif op == '-':
-                p[0] = operator.sub(left, right)
-            elif op == '*':
-                p[0] = operator.mul(left, right)
-            elif op == '/':
-                p[0] = operator.floordiv(left, right)
-            elif op == '^':
-                p[0] = operator.pow(left, right)
-
-        def p_expression_dice(p):
-            """expression : expression DICE expression"""
-            p[0] = rollDice(p[1], p[3])
-
-        def p_expression_uminus(p):
-            """expression : MINUS expression %prec UMINUS"""
-            p[0] = operator.neg(sumList(p[2]))
-
-        def p_expression_udice(p):
-            """expression : DICE expression %prec UDICE"""
-            p[0] = rollDice(1, p[2])
-
-        def p_expression_group(p):
-            """expression : LPAREN expression RPAREN"""
-            p[0] = p[2]
-
-        def p_expression_number(p):
-            """expression : NUMBER"""
-            p[0] = p[1]
-
-        def p_error(p):
-            if p is None:
-                raise SyntaxErrorException(u"syntax error at the end of the given expression")
-
-            col = findColumn(p)
-            raise SyntaxErrorException(u"syntax error at '{0}' (col {1})".format(p.value, col))
-
-        def rollDice(numDice, numSides):
-            numDice = sumList(numDice)
-            numSides = sumList(numSides)
-
-            if numDice > MAX_DICE:
-                raise TooManyDiceException(u'attempted to roll more than {0} dice in a single d expression'.format(MAX_DICE))
-            if numSides > MAX_SIDES:
-                raise TooManySidesException(u'attempted to roll a die with more than {0} sides'.format(MAX_SIDES))
-            if numDice < 0:
-                raise NegativeDiceException(u'attempted to roll a negative number of dice')
-            if numSides < 0:
-                raise NegativeSidesException(u'attempted to roll a die with a negative number of sides')
-            if numSides < 1:
-                raise ZeroSidesException(u'attempted to roll a die with zero sides')
-
-            rolls = []
-            for dice in range(0, numDice):
-                rolls.append(random.randint(1, numSides))
-
-            self.yaccer.rolls.append((u'{0}d{1}'.format(numDice, numSides), rolls))
-
-            return rolls
-
-        def sumList(rolls):
-            if isinstance(rolls, collections.Iterable):
-                return sum(rolls)
-            else:
-                return rolls
-
-        self.yaccer = yacc.yacc()
-        self.yaccer.rolls = []
+        self.roller = DiceUtils.DiceParser()
 
     def execute(self, message):
         """
@@ -209,12 +31,8 @@ class Roll(CommandInterface):
             verbose = True
 
         try:
-            result = self.yaccer.parse(message.Parameters)
-            if isinstance(result, collections.Iterable):
-                result = sum(result)
+            result = self.roller.parse(message.Parameters)
         except OverflowError:
-            self.yaccer.rolls = []
-
             return IRCResponse(ResponseType.Say,
                                u'Error: result too large to calculate',
                                message.ReplyTo)
@@ -226,29 +44,20 @@ class Roll(CommandInterface):
                 NegativeDiceException,
                 NegativeSidesException,
                 ZeroSidesException) as e:
-            self.yaccer.rolls = []
-
             return IRCResponse(ResponseType.Say,
-                               u'Error: {0}'.format(e),
+                               u'Error: {}'.format(e),
                                message.ReplyTo)
 
-        rolls = self.yaccer.rolls
-        self.yaccer.rolls = []
-
         if verbose:
-            rollStrings = []
-            for dice, rollList in rolls:
-                rollStrings.append(u'{0}: {1} ({2})'.format(dice,
-                                                            u','.join(u'{0}'.format(roll) for roll in rollList),
-                                                            sum(rollList)))
+            rollStrings = self.roller.getRollStrings()
             rollString = u' | '.join(rollStrings)
 
             if len(rollString) > 200:
                 rollString = u"LOTS O' DICE"
 
-            response = u'{0} rolled: [{1}] {2}'.format(message.User.Name, rollString, result)
+            response = u'{} rolled: [{}] {}'.format(message.User.Name, rollString, result)
 
         else:
-            response = u'{0} rolled: {1}'.format(message.User.Name, result)
+            response = u'{} rolled: {}'.format(message.User.Name, result)
 
         return IRCResponse(ResponseType.Say, response, message.ReplyTo, {'rollTotal': result})
---
+++ Utils/DiceUtils.py
@@ -0,0 +1,250 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Feb 01, 2018
+
+@author: Tyranic-Moron
+"""
+
+import random
+import operator
+import collections
+from builtins import range
+
+import ply.lex as lex
+from ply.lex import TOKEN
+import ply.yacc as yacc
+
+
+class UnknownCharacterException(Exception):
+    pass
+
+class SyntaxErrorException(Exception):
+    pass
+
+class TooManyDiceException(Exception):
+    pass
+
+class TooManySidesException(Exception):
+    pass
+
+class NegativeDiceException(Exception):
+    pass
+
+class NegativeSidesException(Exception):
+    pass
+
+class ZeroSidesException(Exception):
+    pass
+
+
+class DiceParser(object):
+
+    def __init__(self, maxDice=10000, maxSides=10000):
+        self._MAX_DICE = maxDice
+        self._MAX_SIDES = maxSides
+
+        self.lexer = lex.lex(module=self)
+        self.yaccer = yacc.yacc(module=self)
+
+        self.rolls = []
+
+    def parse(self, diceexpr):
+        self.rolls = []
+
+        result = self.yaccer.parse(diceexpr)
+        if isinstance(result, collections.Iterable):
+            result = sum(result)
+        return result
+
+    def getRollStrings(self):
+        rollStrings = []
+        for dice, rollList in self.rolls:
+            rollStrings.append(u'{}: {} ({})'.format(dice,
+                                                     u','.join(u'{}'.format(roll) for roll in rollList),
+                                                     sum(rollList)))
+        return rollStrings
+
+    tokens = ('NUMBER',
+              'PLUS', 'MINUS',
+              'TIMES', 'DIVIDE',
+              'EXPONENT',
+              'DICE',
+              'LPAREN', 'RPAREN',
+              'POINT')
+
+    # Tokens
+
+    t_PLUS = r'\+'
+    t_MINUS = r'-'
+    t_TIMES = r'\*'
+    t_DIVIDE = r'/'
+    t_EXPONENT = r'\^'
+    t_DICE = r'd'
+    t_LPAREN = r'\('
+    t_RPAREN = r'\)'
+    t_POINT = r'\.'
+
+    @TOKEN(r'\d+')
+    def t_NUMBER(self, t):
+        try:
+            if len(t.value) < 100:
+                t.value = int(t.value)
+            else:
+                raise ValueError
+        except ValueError:
+            t.value = 0
+        return t
+
+    # Ignored characters
+    t_ignore = ' \t'
+
+    @TOKEN(r'\#.*')
+    def t_COMMENT(self, t):
+        pass
+        # No return value. Token discarded
+
+    # Calculate the column position of the given token.
+    #     input is the input text string
+    #     token is a token instance
+    def _findColumn(self, token):
+        if token is not None:
+            last_cr = self.lexer.lexdata.rfind('\n', 0, token.lexpos)
+            if last_cr < 0:
+                last_cr = 0
+            column = (token.lexpos - last_cr) + 1
+            return column
+        else:
+            return 'unknown'
+
+    def t_error(self, t):
+        col = self._findColumn(t)
+        raise UnknownCharacterException(u"unknown character '{}' (col {})".format(t.value[0], col))
+
+
+    # Parsing rules
+    precedence = (('left', 'PLUS', 'MINUS'),
+                  ('left', 'TIMES', 'DIVIDE'),
+                  ('left', 'EXPONENT'),
+                  ('left', 'DICE'),
+                  ('right', 'UMINUS'),
+                  ('right', 'UDICE'))
+
+    def p_statement_expr(self, p):
+        """statement : expression"""
+        p[0] = p[1]
+
+    def p_expression_binop(self, p):
+        """expression : expression PLUS expression
+                      | expression MINUS expression
+                      | expression TIMES expression
+                      | expression DIVIDE expression
+                      | expression EXPONENT expression"""
+
+        op = p[2]
+        left = self._sumList(p[1])
+        right = self._sumList(p[3])
+
+        if op == '+':
+            p[0] = operator.add(left, right)
+        elif op == '-':
+            p[0] = operator.sub(left, right)
+        elif op == '*':
+            p[0] = operator.mul(left, right)
+        elif op == '/':
+            p[0] = operator.floordiv(left, right)
+        elif op == '^':
+            p[0] = operator.pow(left, right)        
+
+    def p_expression_dice(self, p):
+        """expression : expression DICE expression"""
+        p[0] = self._rollDice(p[1], p[3])
+
+    def p_expression_uminus(self, p):
+        """expression : MINUS expression %prec UMINUS"""
+        p[0] = operator.neg(self._sumList(p[2]))
+
+    def p_expression_udice(self, p):
+        """expression : DICE expression %prec UDICE"""
+        p[0] = self._rollDice(1, p[2])
+
+    def p_expression_group(self, p):
+        """expression : LPAREN expression RPAREN"""
+        p[0] = p[2]
+
+    def p_expression_number(self, p):
+        """expression : NUMBER"""
+        p[0] = p[1]
+
+    def p_error(self, p):
+        if p is None:
+            raise SyntaxErrorException(u"syntax error at the end of the given expression")
+
+        col = self._findColumn(p)
+        raise SyntaxErrorException(u"syntax error at '{}' (col {})".format(p.value, col))
+
+    def _rollDice(self, numDice, numSides):
+        numDice = self._sumList(numDice)
+        numSides = self._sumList(numSides)
+
+        if numDice > self._MAX_DICE:
+            raise TooManyDiceException(u'attempted to roll more than {} dice in a single d expression'.format(self._MAX_DICE))
+        if numSides > self._MAX_SIDES:
+            raise TooManySidesException(u'attempted to roll a die with more than {} sides'.format(self._MAX_SIDES))
+        if numDice < 0:
+            raise NegativeDiceException(u'attempted to roll a negative number of dice')
+        if numSides < 0:
+            raise NegativeSidesException(u'attempted to roll a die with a negative number of sides')
+        if numSides < 1:
+            raise ZeroSidesException(u'attempted to roll a die with zero sides')
+
+        rolls = []
+        for dice in range(0, numDice):
+            rolls.append(random.randint(1, numSides))
+
+        self.rolls.append((u'{}d{}'.format(numDice, numSides), rolls))
+
+        return rolls
+
+    def _sumList(self, rolls):
+        if isinstance(rolls, collections.Iterable):
+            return sum(rolls)
+        else:
+            return rolls
+
+
+def main():
+    import argparse
+    argparser = argparse.ArgumentParser(description='An interpreter for dice expressions.')
+    argparser.add_argument('-v', '--verbose', help='print all roll results', action='store_true')
+    argparser.add_argument('diceexpr', help='the dice expression you want to execute', type=str)
+    cmdArgs = argparser.parse_args()
+
+    roller = DiceParser()
+
+    try:
+        result = roller.parse(cmdArgs.diceexpr)
+    except OverflowError:
+        print(u'Error: result too large to calculate')
+    except (ZeroDivisionError,
+            UnknownCharacterException,
+            SyntaxErrorException,
+            TooManyDiceException,
+            TooManySidesException,
+            NegativeDiceException,
+            NegativeDiceException,
+            ZeroSidesException) as e:
+        print(u'Error: {}'.format(e))
+
+    if cmdArgs.verbose:
+        rolls = roller.rolls
+        rollStrings = roller.getRollStrings()
+        rollString = u' | '.join(rollStrings)
+
+        print('[{}] {}'.format(rollString, result))
+        return
+
+    print(result)
+
+
+if __name__ == '__main__':
+    main()
